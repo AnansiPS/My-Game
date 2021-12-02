0459c5fb1b3dc8e1f54de907abbd5d36f13df36a
Anastasya Sherbakova <sherbakova131929@gmail.com>
2021-12-02 22:22:03 +0300

    Working with axes

--- main.py
+++ main.py
@@ -24,3 +24,22 @@ sc = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 # background image
 img = pygame.image.load('e6c220c016bbf85019759f2f0b06cd77.png').convert()
+
+def detect_collision(dx, dy, ball, rect):
+    if dx > 0:
+        delta_x = ball.right - rect.left
+    else:
+        delta_x = rect.right - ball.left
+    if dy > 0:
+        delta_y = ball.bottom - rect.top
+    else:
+        delta_y = rect.bottom - ball.top
+
+    if abs(delta_x - delta_y) < 10:
+        dx, dy = -dx, -dy
+    elif delta_x > delta_y:
+        dy = -dy
+    elif delta_y > delta_x:
+        dx = -dx
+    return dx, dy
+
